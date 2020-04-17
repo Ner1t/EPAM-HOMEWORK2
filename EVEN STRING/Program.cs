6f2aff823b8fccca7927c83f4a72fa969229ddb3
Roman <roman.nerit@gmail.com>
2020-04-17 23:18:12 +0300

    Add project files.

---
+++ EVEN STRING/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace EVEN_STRING
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = "EPAMITDAYS";
+
+            for (int i = 1; i < a.Length; i++)
+
+                a = a.Remove(i, 1);
+
+            Console.WriteLine(a);
+        }
+    }
+}
